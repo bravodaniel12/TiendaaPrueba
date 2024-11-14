427039cc60223d343c868cbc1b80144d4d0bb494
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-14 13:32:17 -0500

    Add dependencies

--- TiendaP/src/App.jsx
+++ TiendaP/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { motion } from "framer-motion";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <motion.button
+          className='w-20 h-20'
+          whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +32,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
     </>
   )
 }
