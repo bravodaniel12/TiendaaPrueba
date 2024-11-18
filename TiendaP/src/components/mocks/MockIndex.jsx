06806d2be3d00be97983f5738b70e5be2ddd1615
Darío José Zamora Vargas <dajozavargas@gmail.com>
2024-11-18 17:41:54 -0500

    Add mock and dynamically render

---
+++ TiendaP/src/components/mocks/MockIndex.jsx
@@ -0,0 +1,33 @@
+export const shops = [
+	{
+		nombre: "Ropa uno",
+		precio: 50000,
+		img: "./vite.svg",
+	},
+	{
+		nombre: "Ropa dos",
+		precio: 10000,
+		img: "./vite.svg",
+	},
+	{
+		nombre: "Ropa tres",
+		precio: 20000,
+		img: "./vite.svg",
+	},
+	{
+		nombre: "Ropa cuatro",
+		precio: 30000,
+		img: "./vite.svg",
+	},
+	{
+		nombre: "Ropa cinco",
+		precio: 200000,
+		img: "./vite.svg",
+	},
+	{
+		nombre: "Ropa seis",
+		precio: 20000,
+		img: "./vite.svg",
+	},
+
+];
---
+++ TiendaP/src/components/pages/Start.jsx
@@ -0,0 +1,38 @@
+import Header from "../Header";
+import { shops } from "../mocks/MockIndex";
+function Start() {
+	return (
+		<>
+			<div className="bg-zinc-100">
+				<div className=" w-full h-screen md:px-40 xs:px-6 py-8">
+          <Header />
+					<h1 className="md:text-5xl text-4xl font-bold text-stone-900 py-5">
+						Tienda ropa 🐼
+					</h1>
+					<div className="grid sm:grid-cols-3 xs:grid-cols-1 gap-8 place-content-between">
+						{shops.map((shop, index) => {
+							const { nombre, precio, img } = shop;
+							return (
+								<div key={index} className="flex justify-center">
+									<div className="overflow-hidden rounded-lg shadow-lg border bg-zinc-200 p-6">
+										<h3 className="text-stone-900 text-3xl font-bold">
+											{nombre}
+										</h3>
+										<p className="font-serif text-stone-900 font-semibold text-lg py-2">
+											${precio}
+										</p>
+										<div className="opacity-100 hover:opacity-75">
+											<img className="w-80 h-72 rounded-lg" src={img} alt={nombre} />
+										</div>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Start;
